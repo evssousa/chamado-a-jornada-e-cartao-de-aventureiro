f4f5dbd1c576b9659eed7c6e463b80db5a17daed
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-05-09 00:57:17 +0000

    ato2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import CartaoDeAventureiro from "./components/CartaoDeAventureiro";
 import MensagemInicial from "./components/MensagemInicial";
 
 export default function App() {
+  const [classe, setClasse] = useState('Aventureiro')
+  const [nivel, setNivel] = useState(1)
+
   return (
     <div>
       <MensagemInicial nome={'Everson'} />
-      <CartaoDeAventureiro nome={'João'} classe={'Dev Fronend'} nivel={1} />
+
+      <div>
+        <h2>Escolha sua Classe:</h2>
+        <button onClick={() => setClasse('Guerreiro')}>Guerreiro</button>
+        <button onClick={() => setClasse('Mago')}>Mago</button>
+        <button onClick={() => setClasse('Curandeiro')}>Curandeiro</button>
+
+        <button onClick={() => setNivel(nivel +=1)}>Subir de Nível</button>
+      </div>
+
+      <CartaoDeAventureiro nome={'João'} classe={'Dev Frontend'} nivel={nivel} />
     </div>
   )
 }
